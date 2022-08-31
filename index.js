b3a58518843e8816cdd372e9d468485dd63d2d39
Horacio Lerda <horacio.lerda@gmail.com>
2022-08-30 21:38:06 -0300

    MCGA-03: fs - add json values

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express')
+const filesystem = require('fs')
 const products = require('./data/products.json')
 const companies = require('./data/companies.json')
 const employees = require('./data/employees.json')
+const { restart } = require('nodemon')
+const { json } = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.json());
+
+app.post("/products/add", (req, res) => {
+  console.log(req.body)
+  const newProd = {
+    // id: "1001",
+    // name: "cherry - rootbeer",
+    // price: "$4.56"
+    id: req.body.id,
+    name: req.body.name,
+    price: req.body.price,
+  };
+  if (!newProd.id){
+    res.sendStatus(400);
+  }
+  products.push(newProd);
+  filesystem.writeFile("./data/products.json", JSON.stringify(products), (err) => {
+    //if (err) { res.sendError(500, 'Error While Saving Data'); }
+  });
+  res.json(newProd);
+});
+
+app.delete("/products/delete", (req, res) => {
+  console.log(req.body)
+  
+})
 
 app.get('/helloWorld', (req, res) => {
   res.send('Hello World!')
