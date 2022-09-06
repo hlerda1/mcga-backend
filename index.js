0e0386bc8170053bdf2d75da9471847aae6d9bb9
Horacio Lerda <horacio.lerda@gmail.com>
2022-09-06 09:54:41 -0300

    MCGA-06: Update value endpoint

--- index.js
+++ index.js
@@ -65,6 +65,23 @@ app.delete("/products/delete/id/:id", (req, res) => {
   res.send(resp)
 })
 
+app.patch("/products/update/id/:id", (req, res) => {
+  let resp = '';
+  let idProduct = parseInt(req.params.id);
+  const index = products.findIndex(function (product) {return product.id == idProduct;});
+  index < 0 ? (resp = 'value not found'):(
+    products[index].id = req.body.id,
+    products[index].name = req.body.name, 
+    products[index].price = req.body.price,
+    products[index].category = req.body.category,
+    products[index].stock = req.body.stock,
+    resp = 'Modified Value: \n' + products[index].id);
+  filesystem.writeFile("./data/products.json", JSON.stringify(products), (err) => {
+    //if (err) { res.sendError(500, 'Error While Saving Data'); }
+  });  
+  res.send(resp)
+})
+
 app.get('/helloWorld', (req, res) => {
   res.send('Hello World!')
 })
@@ -93,7 +110,7 @@ app.get('/employees', (req, res) => {
 })
 
 app.get('/products/:id', (req, res) => {
-  res.json(filtrarProducts(req.params.id))
+  res.json(filterProducts(req.params.id))
 })
 
 app.get('/companies/:id', (req, res) => {
@@ -104,7 +121,7 @@ app.get('/employees/:id', (req, res) => {
   res.json(filtrarEmployees(req.params.id))
 })
 
-const filtrarProducts = (id) => {
+const filterProducts = (id) => {
   let productos = products.filter(prod => prod.id == id)
   return productos  
 }
